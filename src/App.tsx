3c4310d4528ba23084c7bf921f9c74f2a8be221c
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-19 22:41:43 -0500

    2_Styling_Components_Vanilla_CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import { useState } from "react";
+import ListGroup from "./components/ListGroup";
 
 /* NOTES
 
@@ -34,11 +35,19 @@ function App() {
   // onClose cojmes from Alert.tsx which notifies when user clicks on close button. once it's clicked it's set to false
   const [alertVisible, setAlertVisibility] = useState(false);
 
+  // for cities every time you click on a city it logs it in console
+  let items = ["New York", "San Fransisco", "Tokyo", "London"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
   return (
-    <div>
+    <>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
       { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
       <Button color="primary" onClick={() => setAlertVisibility(true)}>My button</Button>
-    </div>
+    </>
   )
 }
 
