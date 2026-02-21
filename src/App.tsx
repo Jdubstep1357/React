4966dda976e0b14423e4070891d9e26724c33c66
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-21 16:01:34 -0500

    REDOING ALL CHANGES INDIVIDUALLY TO KEEP BLOATED FILE

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
-import { useState } from "react";
-import ListGroup from "./components/ListGroup";
-import styled from 'styled-components';
-import "./components/ListGroup/ListGroup.module.css";
-import Like from "./components/Like";
+import Message from "./components/Message";
 
 /* NOTES
 
@@ -40,76 +34,27 @@ import Like from "./components/Like";
     -- Group related variables inside an object
     -- Avoid deeply nested structures
 
+  Pure function
+    -- Given same input, always returns same result
+
 */
 
 
-const ButtonStyle = styled.button`
-  color: red`;
+
 
 
 function App() {
-  
-  // used for heart icon on off - <Like> component
-  const [alertVisible, setAlertVisibility] = useState(false);
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
-
-  // Lesson for Managing Component State 2 - Understanding State Hook
-  let count = 0;
-  
-  const [isVisible, setVisibility] = useState(false);
-  const handleClick = () => {
-    setVisibility(true);
-
-    // will not add due to App calling count from function, which is set to 0 each time called
-    count++;
-    console.log(isVisible);
-  }
-
-  // 3_Managing_Component_State - Choosing State Structure
-
-  // PUT ALL THIS IN PERSON OBJECT
-  // const [firstName, setFirstName] = useState('');
-  // const [lastName, setLastName] = useState('');
-  // const fullName = firstName + ' ' + lastName;
-
-  const [person, setPerson] = useState({
-    firstName: '',
-    lastName: ''
-  })
-
-  const [isLoading, setLoading] = useState(false);
-
-  return (
-    <div>
-    </div>
-  );
 
 
-
-
-  let items = ["New York", "San Fransisco", "Tokyo", "London"];
-
-
-
+// Outputs 4 of the same answer
   return (
     <>
-    <button onClick={handleClick}>Show</button>
-
-
-      {/* <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>My button</Button>
-      <Button onClick={() => {}}>My button</Button> */}
-
-      {/* Like button from Styling Components */}
-      {/* <Like onClick={() => console.log('clicked')} /> */}
-
-      {/* Shows popup based upon other things */}
-      {/* { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>} */}
+      <Message />
+      <Message />
+      <Message />
 
     </>
-  )
+  );
 }
 
 export default App;
