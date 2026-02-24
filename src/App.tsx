b940aa9e29cdd3ec46d8813d99d9b30b89b29ba6
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-23 22:52:20 -0500

    11_Building_Forms_Building_ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -58,13 +58,26 @@
 */
 
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { useState } from "react";
 
 // Lorem100 generates 100 words of lorem ipsum
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+    { id: 5, description: "eee", amount: 10, category: "Utilities" },
+  ]);
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
