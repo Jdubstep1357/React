347c18560b5936a00ef3ffda15b2da193d9aff2b
Jdubstep1357 <jwlyons2015@gmail.com>
2026-02-26 11:40:38 -0500

    2_Connecting_to_Backend_Understanding_Effect_Hook

--- src/App.tsx
+++ src/App.tsx
@@ -52,53 +52,33 @@
     -- 1st render used to detect and reporting issues with code
     -- 2nd render updates user interface
 
+  // Lorem100 generates 100 words of lorem ipsum
+
+  Effect Hooke - Execute a piece of code after a component is rendered
+  useEffect( () => {} )
 
   
 
 */
 
-import Form from "./components/Form";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import { useState } from "react";
-import categories from "./expense-tracker/Categories";
-
-// Lorem100 generates 100 words of lorem ipsum
+import { useEffect, useRef } from "react";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 10, category: "Groceries" },
-    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
-    { id: 5, description: "eee", amount: 10, category: "Utilities" },
-  ]);
+  const ref = useRef<HTMLInputElement>(null);
 
-  // show specific selected categories - if not true then just set it to expenses with everything
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  // afterRenderer
+  useEffect(() => {
+    // side effect - changing something outside of this component
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "My App";
+  });
 
   return (
     <div>
-      <div className="bm-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
 }
